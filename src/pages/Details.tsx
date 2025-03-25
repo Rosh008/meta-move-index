27c7632c5ef78bb2fe27f31bbdf4973c16e7d84e
Roshan Wazare <roshanwazare@gmail.com>
2025-03-25 17:27:45 +0530

    feat:details summary card

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -12,15 +12,16 @@ export default function Details() {
   const tokenId = params.tokenId;
 
   useEffect(() => {
-    if (!tokenId) return;
     setLoading(true);
     const fetchData = async () => {
       const projectData = await fetchProjectDetails();
-      const tokenData = await fetchProjectData(tokenId);
+      const tokenData = await fetchProjectData(tokenId as string);
       if (!tokenData.data.length) return;
       setDetails({ ...projectData, ...tokenData.data[0] });
     };
-    fetchData();
+    if (tokenId) {
+      fetchData();
+    }
     setLoading(false);
   }, []);
 
@@ -40,7 +41,7 @@ export default function Details() {
       <div onClick={onBackClick} className="text-[#2E47E0] mb-3 cursor-pointer">
         Back
       </div>
-      <div className="flex flex-col lg:flex-row w-full bg-[#171717] lg:justify-between p-4">
+      <div className="flex flex-col items-center lg:flex-row w-full bg-[#171717] lg:justify-between p-4">
         <div className="flex gap-2">
           <img
             className="h-16 w-16"
@@ -48,18 +49,81 @@ export default function Details() {
             src={details.info.imageUrl}
           />
           <div className="flex flex-col gap-2">
-            <span>{details.projectName}</span>
-            <div>
-              <div className="p-2 px-3 text-truncate border rounded-lg">
-                {details.contractAddress}
+            <span className="text-lg">{details.projectName}</span>
+            <div className="flex items-center gap-3 border border-[#383838] border-0.5">
+              <div className="p-2 px-3 text-[14px] text-[#A2A2A2]">
+                {`${details.contractAddress.slice(
+                  0,
+                  6
+                )}...${details.contractAddress.slice(
+                  details.contractAddress.length - 4
+                )}`}
               </div>
-              <a>Copy button</a>
+              <div
+                className="mr-4"
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText(details.contractAddress)
+                    .then(() => alert("Address copied"));
+                }}
+              >
+                <img
+                  alt="copy"
+                  width="14"
+                  height="14"
+                  className="cursor-pointer brightness-0 invert opacity-60 hover:opacity-100"
+                  src="https://www.goatindex.ai/images/icons/copy.svg"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col">
-          <div>About</div>
-          <div>Links</div>
+        <div className="flex flex-col mt-4 lg:mt-0">
+          <div className="text-sm text-[#A2A2A2]">About</div>
+          <span className="max-w-md truncate whitespace-normal text-sm text-[#A2A2A2]">
+            {details.description}
+          </span>
+        </div>
+        <div className="flex mr-3 gap-2 mt-4 lg:mt-0">
+          <p className="text-sm">Links</p>
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className="flex gap-2 items-center"
+          >
+            <a
+              target="_blank"
+              href={`https://twitter.com/${details.twitterHandle}`}
+            >
+              <img
+                alt="twitter"
+                className="w-6 h-6 bg-grey-50"
+                src="https://www.goatindex.ai/images/icons/twitter.svg"
+              />
+            </a>
+            <a target="_blank" href={`${details.telegramLink}`}>
+              <img
+                alt="telegram"
+                className="w-6 h-6 bg-grey-50"
+                src="	https://www.goatindex.ai/images/icons/telegram.svg"
+              />
+            </a>
+            <a target="_blank" href={`${details.websiteLink}`}>
+              <img
+                alt="website"
+                className="w-6 h-6 bg-grey-50"
+                src="	https://www.goatindex.ai/images/icons/website.svg"
+              />
+            </a>
+            <a target="_blank" href={`${details.url}`}>
+              <img
+                alt="dex screener"
+                className="w-6 h-6 bg-grey-50"
+                src="https://www.goatindex.ai/images/icons/dexscreener.svg"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen w-screen">
       <header className="w-full fixed py-4 px-6 flex justify-between items-center bg-[#171717] top-0 z-50">
-        <div className="font-serif text-lg">Meta Move AI</div>
+        <div className="font-serif text-lg">AptIndex AI</div>
         <Button variant="outline">Submit request</Button>
       </header>
       <main className="w-full flex-1 xl:px-24 px-8 mx-auto mt-24">
@@ -15,7 +15,7 @@ export default function Layout({ children }: LayoutProps) {
       </main>
       <footer className="xl:px-24 px-8 my-4">
         <div className="text-[#A2A2A2] text-center text-sm ">
-          Copyright © 2025 Meta Move AI
+          Copyright © 2025 AptIndex AI
         </div>
       </footer>
     </div>
